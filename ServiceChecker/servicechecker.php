029cad3b82bc570d7316da51514b0f4b8a7d95c5
nuria <naturalis@users.noreply.github.com>
2009-07-21 14:53:58 +0000

    branching current version

---
+++ ServiceChecker/servicechecker.php
@@ -0,0 +1,40 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Species 2000 Service Checker</title>
+<meta name="title" content="Welcome to Species 2000" />
+<meta name="robots" content="index, follow" />
+	<link rel="shortcut icon" href="http://reading.sp2000.org/joomla/images/favicon.ico" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="http://reading.sp2000.org/joomla/templates/sp2ktemplate/css/template_css.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+<img src="http://www.sp2000.org/templates/sp2ktemplate/images/WebBanner4.jpg"><br><br>
+
+<p>This page shows the status of the Species 2000 services.</p>
+
+<h2>Websites</h2>
+<p><b><h3><font color="red">Species 2000 website</font></h3></b> <br> <b>URL</b> - <a href="http://www.sp2000.org" target="_blank">www.sp2000.org...</a> <br> <b>Status</b> - Website online - but the reference file against which it's response is tested is missing.<br><b>Location</b> - Reading</br></p>
+<p><b><h3><font color="red">Species 2000 europa website</font></h3></b> <br> <b>URL</b> - <a href="http://www.sp2000europa.org" target="_blank">www.sp2000europa.org...</a> <br> <b>Status</b> - Website online - but the reference file against which it's response is tested is missing.<br><b>Location</b> - Thompson</br></p>
+<br />
+<b>Warning</b>:  file_get_contents(http://metadb.sp2000.org/mdbDatabase.php) [<a href='function.file-get-contents'>function.file-get-contents</a>]: failed to open stream: HTTP request failed! HTTP/1.1 404 Not Found
+ in <b>/mnt/biodiversity/sda5/public_html/repository/sp2000/20061107/Websites/ServiceChecker/servicechecker.php</b> on line <b>185</b><br />
+<p><b><h3><font color="red">Species 2000 metadatabase</font></h3></b> <br> <b>URL</b> - <a href="http://metadb.sp2000.org/mdbDatabase.php" target="_blank">metadb.sp2000.org/mdbDatabase.php...</a> <br> <b>Status</b> - Website online - but the reference file against which it's response is tested is missing.<br><b>Location</b> - Reading</br></p>
+
+<h2>Checklists</h2>
+
+<p><b><h3><font color="red">Annual Checklist 2006</font></h3></b> <br> <b>URL</b> - <a href="http://annual.sp2000.org/2006/search_results.php?search_string=Abrus+precatorius&amp;match_whole_words=on" target="_blank">annual.sp2000.org/2006/search_results.ph...</a> <br> <b>Status</b> - Checklist online - but the reference file against which it's response is tested is missing.<br><b>Location</b> - Reading</br></p>
+<p><b><h3><font color="red">Dynamic Checklist - Thompson</font></h3></b> <br> <b>URL</b> - <a href="http://spice.sp2000.org/show_species_details.php?record_id=1571&amp;database_id=ILDIS&amp;search_string=Abrus+precatorius&amp;hub=GlobalHub" target="_blank">spice.sp2000.org/show_species_details.ph...</a> <br> <b>Status</b> - Checklist online - but the reference file against which it's response is tested is missing.<br><b>Location</b> - Thompson</br></p>
+<p><b><h3><font color="red">Dynamic Checklist - Reading</font></h3></b> <br> <b>URL</b> - <a href="http://dynamic.sp2000.org/browse_taxa.php" target="_blank">dynamic.sp2000.org/browse_taxa.php...</a> <br> <b>Status</b> - Checklist online - but the reference file against which it's response is tested is missing.<br><b>Location</b> - Reading</br></p>
+
+
+<!--<h2>Wrappers</h2>-->
+
+
+
+
+
+
+
+
