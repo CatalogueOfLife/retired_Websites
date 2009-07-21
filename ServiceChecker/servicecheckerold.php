52bce7b2c1c430758ec177563d1f29b74f1c48e1
nuria <naturalis@users.noreply.github.com>
2009-07-21 14:46:21 +0000

    initial commit

---
+++ ServiceChecker/servicecheckerold.php
@@ -0,0 +1,43 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Species 2000 Service Checker</title>
+<meta name="title" content="Welcome to Species 2000" />
+<meta name="robots" content="index, follow" />
+	<link rel="shortcut icon" href="http://reading.sp2000.org/joomla/images/favicon.ico" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="http://reading.sp2000.org/joomla/templates/sp2ktemplate/css/template_css.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+<img src="http://reading.sp2000.org/joomla/templates/sp2ktemplate/images/WebBanner4.jpg"><br><br>
+
+<p>This page shows the status of the Species 2000 services.</p>
+
+<h2>Websites</h2>
+<p><b><h3><font color="red">Species 2000 website</font></h3></b> <br> <b>URL</b> - <a href="http://www.sp2000.org" target="_blank">www.sp2000.org...</a> <br> <b>Status</b> - error - reference file missing<br></p>
+<p><b><h3><font color="red">Species 2000 europa website</font></h3></b> <br> <b>URL</b> - <a href="http://www.sp2000europa.org" target="_blank">www.sp2000europa.org...</a> <br> <b>Status</b> - error - reference file missing<br></p>
+<p><b><h3><font color="red">University of Reading website</font></h3></b> <br> <b>URL</b> - <a href="http://www.rdg.ac.uk" target="_blank">www.rdg.ac.uk...</a> <br> <b>Status</b> - error - reference file missing<br></p>
+
+<h2>Checklists</h2>
+
+<p><b><h3><font color="red">Annual Checklist 2006</font></h3></b> <br> <b>URL</b> - <a href="http://annual.sp2000.org/2006/search_results.php?search_string=Abrus+precatorius&match_whole_words=on" target="_blank">annual.sp2000.org/2006/search_results.ph...</a> <br> <b>Status</b> - error - reference file missing<br></p>
+
+
+
+
+<!--<h2>Wrappers</h2>-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
